6f28b8da8e0e6cf9c986db54c12a24a264f98168
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-10 11:42:20 +0200

    Some more settings

--- api/settings.py
+++ api/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = []
 
 
 # Application definition
