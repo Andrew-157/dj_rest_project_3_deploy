06bdfa4c9a64d8519111d5c3f0e41c6416390478
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-23 11:32:42 +0200

    Changes in image size validations

--- recipes/validators.py
+++ recipes/validators.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 
 def validate_file_size(image):
 
-    file_size = image.file.size
+    file_size = image.size
     limit_mb = 5
     if file_size > limit_mb * 1024 * 1024:
         raise ValidationError(f"Maximum size of the image is {limit_mb} MB")
